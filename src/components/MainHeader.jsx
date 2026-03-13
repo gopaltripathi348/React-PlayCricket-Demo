8feb14f37328b12f7fc89d0451a067e69b183e95
gopaltripathi348 <Gopal.Tirpathi@ecb.co.uk>
2026-03-13 14:02:20 +0530

    Update MainHeader.jsx

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -2,7 +2,7 @@ import React,{useEffect, useState} from 'react';
 import TopHeader from './TopHeader';
 import Header from './Header';
 import topStyles from "./TopHeader.module.css";
-import styles from './header.module.css';
+import styles from './Header.module.css';
 import "../styles/styles.css";
 import Slider from './Slider';
 
